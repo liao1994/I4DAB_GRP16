e609888e5d4caec7db9a383d0d9147ad3273fcb5
Msknudsen <m_s_knudsen@hotmail.com>
2016-04-08 11:35:00 +0200

    Adresse og Phone

--- DAB_Handin4/DAB_Handin4_APP/Models/Adresse.cs
+++ DAB_Handin4/DAB_Handin4_APP/Models/Adresse.cs
@@ -8,5 +8,23 @@ namespace DAB_Handin4_APP.Models
 {
     public class Adresse
     {
+        [Key]
+        [Required]
+        public int AdressID { get; set; }
+        
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string StreetName { get; set; }
+
+        [Required]
+        public int StreetNumber { get; set; }
+
+        [Required]
+        public string Town { get; set; }
+
+        [Required]
+        public int ZipCode { get; set; }
     }
 }
--- DAB_Handin4/DAB_Handin4_APP/Models/Phone.cs
+++ DAB_Handin4/DAB_Handin4_APP/Models/Phone.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,20 @@ namespace DAB_Handin4_APP.Models
 {
     public class Phone
     {
+        [Key]
+        [Required]
+        public int PhoneID { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+        
+        [Required]
+        public int CountryCode { get; set; }
+
+        [Required]
+        public string PhoneType { get; set; }
+
+        [Required]
+        public int PersonID { get; set; }
     }
 }
