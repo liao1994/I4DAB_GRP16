b85a4c91ca6d37bacab1d9debedfcd6969a9254b
Ao Li <201407737@post.au.dk>
2016-04-08 11:17:13 +0200

    Migration + people controller

---
+++ DAB_Handin4/DAB_Handin4_APP/Models/DAB_Handin4_APPContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DAB_Handin4_APP.Models
+{
+    public class DAB_Handin4_APPContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public DAB_Handin4_APPContext() : base("name=DAB_Handin4_APPContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<DAB_Handin4_APP.Models.Person> People { get; set; }
+    }
+}
