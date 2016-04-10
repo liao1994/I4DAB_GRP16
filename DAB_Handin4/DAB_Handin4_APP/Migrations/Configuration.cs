5d7f4f06537af9232a49e18d33a518602808ffca
Msknudsen <m_s_knudsen@hotmail.com>
2016-04-10 14:57:23 +0200

    Adresse + Phone

--- DAB_Handin4/DAB_Handin4_APP/Migrations/Configuration.cs
+++ DAB_Handin4/DAB_Handin4_APP/Migrations/Configuration.cs
@@ -1,3 +1,7 @@
+using DAB_Handin4_APP.Models;
+using DbMigration = System.Data.Entity.Migrations.DbMigration;
+using DbMigrationsConfiguration = System.Data.Entity.Migrations.DbMigrationsConfiguration;
+
 namespace DAB_Handin4_APP.Migrations
 {
     using System;
@@ -26,6 +30,22 @@ namespace DAB_Handin4_APP.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            {
+                context.Adresses.AddOrUpdate(x => x.AdressID,
+                    new Adresse() { AdressID = 1, StreetName = "Finlandsgade",  StreetNumber = 22, Town = "Aarhus", ZipCode = 8210, Country = "Denmark" },
+                    new Adresse() { AdressID = 2, StreetName = "Katrinebjergvej", StreetNumber = 10, Town = "Aarhus", ZipCode = 8210, Country = "Denmark" },
+                    new Adresse() { AdressID = 3, StreetName = "Langelandsgade", StreetNumber = 189, Town = "Aarhus", ZipCode = 8210, Country = "Denmark" }
+                    );
+
+                context.Phones.AddOrUpdate(x => x.PhoneID,
+                    new Phone() {PhoneID = 1, PhoneNumber = 12345678, CountryCode = 45, PhoneType = "Mobil", PersonID = 1},
+                    new Phone() { PhoneID = 2, PhoneNumber = 87654321, CountryCode = 45, PhoneType = "Mobil", PersonID = 2 },
+                    new Phone() { PhoneID = 3, PhoneNumber = 88888888, CountryCode = 45, PhoneType = "Mobil", PersonID = 3 }
+
+                    );
+
+            }
         }
     }
 }
