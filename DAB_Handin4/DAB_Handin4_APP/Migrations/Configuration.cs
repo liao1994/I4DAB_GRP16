e86197e0e70bc86c6dc58d7f8f2cdcd3fa047040
Ao Li <201407737@post.au.dk>
2016-04-11 13:29:28 +0200

    auto gen filer

--- DAB_Handin4/DAB_Handin4_APP/Migrations/Configuration.cs
+++ DAB_Handin4/DAB_Handin4_APP/Migrations/Configuration.cs
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-using System.Web.WebPages;
-using DAB_Handin4_APP.Models;
-=======
+
 using DAB_Handin4_APP.Models;
 using DbMigration = System.Data.Entity.Migrations.DbMigration;
 using DbMigrationsConfiguration = System.Data.Entity.Migrations.DbMigrationsConfiguration;
->>>>>>> origin/master
+
 
 namespace DAB_Handin4_APP.Migrations
 {
@@ -37,30 +34,25 @@ namespace DAB_Handin4_APP.Migrations
             //    );
             //
 
-<<<<<<< HEAD
+
             context.People.AddOrUpdate(
                 x => x.PersonID,
                 new Person() {ForNavn = "test",EfterNavn = "testersen",Relation = "N/A INFO"});
      
-
-   
-=======
-            {
                 context.Adresses.AddOrUpdate(x => x.AdressID,
                     new Adresse() { AdressID = 1, StreetName = "Finlandsgade",  StreetNumber = 22, Town = "Aarhus", ZipCode = 8210, Country = "Denmark" },
                     new Adresse() { AdressID = 2, StreetName = "Katrinebjergvej", StreetNumber = 10, Town = "Aarhus", ZipCode = 8210, Country = "Denmark" },
                     new Adresse() { AdressID = 3, StreetName = "Langelandsgade", StreetNumber = 189, Town = "Aarhus", ZipCode = 8210, Country = "Denmark" }
-                    );
+                );
 
                 context.Phones.AddOrUpdate(x => x.PhoneID,
                     new Phone() {PhoneID = 1, PhoneNumber = 12345678, CountryCode = 45, PhoneType = "Mobil", PersonID = 1},
                     new Phone() { PhoneID = 2, PhoneNumber = 87654321, CountryCode = 45, PhoneType = "Mobil", PersonID = 2 },
                     new Phone() { PhoneID = 3, PhoneNumber = 88888888, CountryCode = 45, PhoneType = "Mobil", PersonID = 3 }
+                );
 
-                    );
+            
 
-            }
->>>>>>> origin/master
         }
     }
 }
